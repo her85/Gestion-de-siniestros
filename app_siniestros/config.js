19eb571ebcb8e899b521d04fdf97e0f62c743a77
HerHub <hab_85@live.com.ar>
2025-03-08 12:14:15 -0300

    test

--- app_siniestros/config.js
+++ app_siniestros/config.js
@@ -3,8 +3,8 @@
 const { config } = require("dotenv");
 config();
 
-const MONGODB_USR=hermondev85
-const MONGODB_PWD=KMlOZSiOG8AY3sVI
+const MONGODB_USR="hermondev85";
+const MONGODB_PWD="KMlOZSiOG8AY3sVI";
 
 //const MONGODB_USR = process.env.MONGODB_USR;
 //const MONGODB_PWD = process.env.MONGODB_PWD;
